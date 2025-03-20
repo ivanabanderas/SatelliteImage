1e39c189d0d9099f4b70e39e807e32c01995bf19
Vanessa Cerda Carrillo <vcc1708.05@gmail.com>
2025-03-20 07:18:39 -0600

    Actualizar ImageProc.py con el código para detectar calles
    El código detecta las calles de la imagen "satelite2.jpg" correctamente.

--- ImageProc.py
+++ ImageProc.py
@@ -3,26 +3,29 @@ import matplotlib.pyplot as plt #Para visualizar las imagenes
 import numpy as np
 
 #Cargar la imagen desde su ruta
-img = cv.imread('imagen.jpg') 
+img = cv.imread('satelite2.jpg') 
 assert img is not None, "file could not be read, check with os.path.exists()"
 
-#Aplica un filtro Gaussiano para reducir el ruido
-blur = cv.GaussianBlur(img, (31, 31), 0)
+# procesamiento
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+black_pixels = np.where(gray <= 20, 255, 0).astype(np.uint8)
+kernel = np.ones((5, 5), np.uint8)
+morph_gradient = cv.morphologyEx(black_pixels, cv.MORPH_GRADIENT, kernel)
+blur = cv.GaussianBlur(morph_gradient, (29,29), 0)
+_, thresh = cv.threshold(blur, 30, 255, cv.THRESH_BINARY_INV)
 
-#Crea la figura para poder procesar la imagen
-plt.figure(figsize=(12, 6))
+# dibujo
+lines = cv.HoughLinesP(thresh, 1, np.pi / 180, threshold=200, minLineLength=100, maxLineGap=20)
+filtered_lines_img = img.copy()
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv.line(filtered_lines_img, (x1, y1), (x2, y2), (128, 0, 255), 2)
 
-#Muestra la imagen original
-plt.subplot(1, 3, 1)
-plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-plt.title("Original")
-plt.xticks([]), plt.yticks([])
+# mostrar
+plt.figure(figsize=(8, 8))
 
-#Muestra la imagen con el filtro correcto
-plt.subplot(1, 3, 2)
-plt.imshow(blur, cmap='gray')
-plt.title("Procesada")
-plt.xticks([]), plt.yticks([])
-
-#Muestra todas las imagenes
+plt.imshow(cv.cvtColor(filtered_lines_img, cv.COLOR_BGR2RGB))
+plt.title("Líneas Filtradas de las Calles")
+plt.axis("off")
 plt.show()
